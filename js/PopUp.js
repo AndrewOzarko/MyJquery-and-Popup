86a2a0f9bcbe42cfbc186839c89d3797944fcd17
Andrew Ozarko <andrewozarko@gmail.com>
2018-07-19 23:26:56 +0300

    Add params in Popup class

--- js/PopUp.js
+++ js/PopUp.js
@@ -1,16 +1,22 @@
-function Popup() {
+function Popup(popupSel, popupLayerSel) {
 
-    var popup = document.querySelector('.popup');
-    var popupLayer = document.querySelector('.popup-layer');
+    /**
+     * Типу інкапсуляція
+     * Змінні не доступні зовні але за рахунок замикань
+     * доступні в середені
+     */
+
+    var popupSel = document.querySelector(popupSel);
+    var popupLayer = document.querySelector(popupLayerSel);
 
     this.open = function(content) {
         popupLayer.style.display = 'block';
-        popup.style.display = 'block';
-        popup.innerHTML = content;
+        popupSel.style.display = 'block';
+        popupSel.innerHTML = content;
     };
 
     this.close = function() {
         popupLayer.style.display = 'none';
-        popup.style.display = 'none';
+        popupSel.style.display = 'none';
     };
 };
